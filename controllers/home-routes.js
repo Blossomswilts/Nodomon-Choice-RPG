8946839534a85630b19e77bea34c89742d8331e6
CaffeinatedJitterBug <itwallaby@gmail.com>
2023-07-13 20:28:55 -0400

    Added user and home routes. Added files for questions and answer routes.

---
+++ controllers/home-routes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const { Character } = require('../models');
+
+router.get('/', async (req, res) => {
+    res.render('homepage');
+})
+
+router.get('/adventure', async (req, res) => {
+    if (!req.session.loggedIn) {
+        res.redirect('/login');
+    }
+    res.render('adventure');
+});
+
+router.get('/character', async (req, res) => {
+    if (!req.session.loggedIn) {
+        res.redirect('/login');
+    }
+    res.render('character');
+});
+
+router.get('/character/:id', async (req, res) => {
+    const donomonData = await Character.findByPk(req.params.id, {
+
+    });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
