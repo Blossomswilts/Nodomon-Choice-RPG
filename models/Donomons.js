d66f09924592acda18fccaf9ddcc025412018d67
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-07-13 21:25:54 -0400

    added donomon table

---
+++ models/Donomons.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Create a table to hold Donomons
+
+class Donomon extends Model {}
+
+Donomon.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    level: {
+      type: DataTypes.INTEGER,
+      DEFAULT: 1,
+      allowNull: false,
+    },
+    exp: {
+      type: DataTypes.INTEGER,
+      DEFAULT: 0,
+      allowNull: false,
+    },
+    morality: {
+      type: DataTypes.INTEGER,
+      DEFAULT: 0,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'donomon',
+  },
+);
