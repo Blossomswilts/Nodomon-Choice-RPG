4b47de106c95d3f886a2ea8301480ecbbc0a7a75
Michael Tranquillo <michael.r.tranquillo@gmail.com>
2023-07-18 18:42:21 -0400

    completed evolve function in helpers

--- utils/helpers.js
+++ utils/helpers.js
@@ -9,6 +9,7 @@ const evolve = async (donomon) => {
     let type;
     // Evolve depending on name and morality
     switch (donomon.name) {
+        //________________________Water Donomon________________________
         case 'Turbulin':
             if (morality >= 0) {
                 name = 'Truskblind';
@@ -16,6 +17,168 @@ const evolve = async (donomon) => {
             } else {
                 name = 'Trushblin';
                 type = 'water/dark';
+            }
+            await Donomon.update(
+                {
+                    type,
+                    name,
+                },
+                {
+                    where: {
+                        id,
+                    },
+                },
+            );
+            break;
+        case 'Trushblin':
+            if (morality >= 0) {
+                name = 'Triskloon';
+                type = 'water/cloud';
+            } else {
+                name = 'Trillnight';
+                type = 'water/midnight';
+            }
+            await Donomon.update(
+                {
+                    type,
+                    name,
+                },
+                {
+                    where: {
+                        id,
+                    },
+                },
+            );
+            break;
+        case 'Truskblind':
+            if (morality >= 0) {
+                name = 'Trushblinding';
+                type = 'water/radiant';
+            } else {
+                name = 'Trisklown';
+                type = 'water/cloud';
+            }
+            await Donomon.update(
+                {
+                    type,
+                    name,
+                },
+                {
+                    where: {
+                        id,
+                    },
+                },
+            );
+            break;
+        //________________________Fire Donomon________________________
+        case 'Raskat':
+            if (morality >= 0) {
+                name = 'Roskight';
+                type = 'fire/light';
+            } else {
+                name = 'Riskin';
+                type = 'fire/dark';
+            }
+            await Donomon.update(
+                {
+                    type,
+                    name,
+                },
+                {
+                    where: {
+                        id,
+                    },
+                },
+            );
+            break;
+        case 'Roskight':
+            if (morality >= 0) {
+                name = 'Raskatiant';
+                type = 'fire/radiant';
+            } else {
+                name = 'Rosekin';
+                type = 'fire/cloud';
+            }
+            await Donomon.update(
+                {
+                    type,
+                    name,
+                },
+                {
+                    where: {
+                        id,
+                    },
+                },
+            );
+            break;
+        case 'Riskin':
+            if (morality >= 0) {
+                name = 'Remskyte';
+                type = 'fire/cloud';
+            } else {
+                name = 'Risknuit';
+                type = 'fire/midnight';
+            }
+            await Donomon.update(
+                {
+                    type,
+                    name,
+                },
+                {
+                    where: {
+                        id,
+                    },
+                },
+            );
+            break;
+        //________________________Grass Donomon_______________________
+        case 'Snoober':
+            if (morality >= 0) {
+                name = 'Snipight';
+                type = 'grass/light';
+            } else {
+                name = 'Snarker';
+                type = 'grass/dark';
+            }
+            await Donomon.update(
+                {
+                    type,
+                    name,
+                },
+                {
+                    where: {
+                        id,
+                    },
+                },
+            );
+            break;
+        case 'Snipight':
+            if (morality >= 0) {
+                name = 'Snoobeam';
+                type = 'grass/radiant';
+            } else {
+                name = 'Snowsyke';
+                type = 'grass/cloud';
+            }
+            await Donomon.update(
+                {
+                    type,
+                    name,
+                },
+                {
+                    where: {
+                        id,
+                    },
+                },
+            );
+            break;
+        case 'Snarker':
+            if (morality >= 0) {
+                name = 'Snawitch';
+                type = 'grass/cloud';
+            } else {
+                name = 'Snawitchin';
+                type = 'grass/midnight';
             }
             await Donomon.update(
                 {
