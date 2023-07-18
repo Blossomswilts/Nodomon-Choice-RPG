ea4fe5d37b97d064c1670c8e2794e6003f634da7
elmiquea <120135663+elmiquea@users.noreply.github.com>
2023-07-18 18:14:03 -0400

    added character pg

---
+++ public/js/adventure.js
@@ -0,0 +1,46 @@
+// Get references to the dropdown and locations list elements
+const dropdown = document.querySelector('#dropdown');
+const locationsList = document.querySelector('ul');
+
+// Add an event listener to the dropdown element
+dropdown.addEventListener('change', (event) => {
+    // Get the selected value from the dropdown
+    const selectedValue = event.target.value;
+
+    // Update the locations list based on the selected value
+    if (selectedValue === 'Questions') {
+    // If the selected value is "Questions", show the questions
+        locationsList.innerHTML = `
+      <li></li>
+      <li></li>
+      <li></li>
+    `;
+    } else if (selectedValue === 'Answers') {
+    // If the selected value is "Answers", show the answers
+        locationsList.innerHTML = `
+      <li></li>
+      <li></li>
+      <li></li>
+    `;
+    } else if (selectedValue === 'Next Evolve') {
+    // If the selected value is "Next Evolve", show the next evolve options
+        locationsList.innerHTML = `
+      <li></li>
+      <li></li>
+      <li></li>
+    `;
+    }
+});
+
+
+// Unhide answers that go with the appropriate questions
+let allAnswers = document.querySelectorAll('option[data-qid]');
+
+allAnswers.forEach((ans) => {
+    let answersQuestion = ans.dataset.qid;
+    let parentQ = ans.parentNode.parentNode.dataset.id;
+
+    if (answersQuestion === parentQ) {
+        ans.style.display = 'block';
+    }
+});
