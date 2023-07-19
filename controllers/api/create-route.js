c2947c75eae27c5eef785f0d8875a051f787afbf
CaffeinatedJitterBug <itwallaby@gmail.com>
2023-07-18 20:52:39 -0400

    Finished create-route.js

--- controllers/api/create-route.js
+++ controllers/api/create-route.js
@@ -1,3 +1,8 @@
+const router = require('express').Router();
+const { Donomon } = require('../../models');
+
 router.post('/create', async (req, res) => {
     const newDonomon = await Donomon.create(req.body);
 });
+
+module.exports = router;
